81e74695361f171fa9ac5ba9ac064bc3168cce93
sasquatch <89185962+SourasishBasu@users.noreply.github.com>
2024-06-15 13:23:00 +0530

    Refactored code, Added comments

--- app/main.py
+++ app/main.py
@@ -4,71 +4,131 @@ import threading
 import argparse
 import gzip
 
+# Default directory for serving files
 BASE_DIRECTORY = './files/'
+
+# HTTP header contents declared
+CONTENT_TYPE_PLAIN = "Content-Type: text/plain\r\n"
+CONTENT_TYPE_OCTET_STREAM = "Content-Type: application/octet-stream\r\n"
+HTTP_OK = "HTTP/1.1 200 OK\r\n"
+HTTP_NOT_FOUND = "HTTP/1.1 404 Not Found\r\n"
+HTTP_CREATED = "HTTP/1.1 201 Created\r\n"
+ENCODING_HEADER = "Content-Encoding: gzip\r\n"
 
 
 def send_response(req):
+    """
+    Returns encoded response based on the endpoint the client socket makes the request to.
+
+    Available Endpoints:
+    - /
+    - /echo
+    - /user-agent
+    - /files
+
+    :param req:
+    :return res:
+    """
+
+    # Parsing HTTP request to get the request line
     status_lines = req.split("\r\n")
-    # print(status_lines)
     method, path, _ = status_lines[0].split(" ")
 
+    # Responding with 200 OK to client if connection to server is successful
     if path == "/":
-        resp = "HTTP/1.1 200 OK\r\n\r\n".encode()
+        res = HTTP_OK + "\r\n"
 
+    # /echo
     elif path.startswith('/echo'):
+
+        # Extracting data from endpoint path in request
         _, info = path[1:].split("/")
 
-        if status_lines[2].startswith('Accept-Encoding'):
+        # Checking for valid compression options from status lines (gzip)
+        if status_lines[2].lower().startswith('accept-encoding'):
             _, compressions = status_lines[2].split(": ")
 
             if 'gzip' in (compressions.split(', ')):
                 body = gzip.compress(info.encode())
-                resp = (b"HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: "
-                        + str(len(body)).encode()
-                        + b"\r\n\r\n"
-                        + body
-                        )
+
+                return (HTTP_OK.encode()
+                        + ENCODING_HEADER.encode()
+                        + CONTENT_TYPE_PLAIN.encode()
+                        + f"Content-Length: {len(body)}\r\n\r\n".encode()
+                        + body)
             else:
-                resp = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(info)}\r\n\r\n{info}".encode()
+                res = (HTTP_OK
+                       + CONTENT_TYPE_PLAIN
+                       + f"Content-Length: {len(info)}\r\n\r\n"
+                       + info)
 
         else:
-            resp = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(info)}\r\n\r\n{info}".encode()
+            res = (HTTP_OK
+                   + CONTENT_TYPE_PLAIN
+                   + f"Content-Length: {len(info)}\r\n\r\n"
+                   + info)
 
+    # /user-agent
     elif path.startswith('/user-agent'):
-        _, agent = status_lines[2].split(" ")
-        resp = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(agent)}\r\n\r\n{agent}".encode()
 
+        # Extracting client's user-agent from request status lines
+        _, agent = status_lines[2].split(" ")
+
+        res = (HTTP_OK
+               + CONTENT_TYPE_PLAIN
+               + f"Content-Length: {len(agent)}\r\n\r\n"
+               + agent)
+
+    # /files
     elif path.startswith('/files'):
         _, file = path[1:].split("/")
-        if method == 'GET':
+
+        # Checking the request method and serving/writing to correct file
+        if method.lower() == 'get':
             if os.path.exists(os.path.join(BASE_DIRECTORY, file)):
                 with open(f'{BASE_DIRECTORY}{file}', 'r') as f:
                     contents = f.read()
-                    resp = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(contents)}\r\n\r\n{contents}".encode()
-            else:
-                resp = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
 
-        elif method == 'POST':
+                    res = (HTTP_OK
+                           + CONTENT_TYPE_OCTET_STREAM
+                           + f"Content-Length: {len(contents)}\r\n\r\n"
+                           + contents)
+
+            else:
+                res = HTTP_NOT_FOUND + "\r\n"
+
+        elif method.lower() == 'post':
             with open(f'{BASE_DIRECTORY}{file}', 'w') as f:
                 f.write(status_lines[4])
-                resp = f"HTTP/1.1 201 Created\r\n\r\n".encode()
 
+                res = HTTP_CREATED + "\r\n"
+
+    # invalid endpoint
     else:
-        resp = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
+        res = HTTP_NOT_FOUND + "\r\n"
 
-    return resp
+    # Return the response encoded in byte form
+    return res.encode()
 
 
 def handle_client(client_socket, address):
+    """
+    Helper function for receiving requests and sending responses between the client and server.
+    
+    :param client_socket: 
+    :param address: 
+    """
     print(f"Connection established to address {address} from server.")
 
     req = client_socket.recv(1024).decode()
-    resp = send_response(req)
-    client_socket.send(resp)
+    res = send_response(req)
+    client_socket.send(res)
     client_socket.close()
 
 
 def main():
+    
+    # --directory argument to add local directory PATH for serving/storing files while starting server
     parser = argparse.ArgumentParser(description="Simple HTTP Web server")
     parser.add_argument('-d', '--directory', type=str, help='Directory address on server for requesting files')
     args = parser.parse_args()
@@ -76,7 +136,7 @@ def main():
     global BASE_DIRECTORY
     BASE_DIRECTORY = args.directory
 
-    # reuse_port to be removed for running on windows
+    # reuse_port to be removed for running on Windows
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.listen()
     print("Server is running on port 4221...")
@@ -85,6 +145,8 @@ def main():
         while True:
             print("Waiting for connection")
             client_socket, addr = server_socket.accept()
+
+            # Starting new threads to handle multiple concurrent clients if needed
             thread = threading.Thread(target=handle_client, args=(client_socket, addr))
             thread.start()
 
